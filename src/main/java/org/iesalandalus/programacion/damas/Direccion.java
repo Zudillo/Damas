f404889ce887d71295af6c415e1c6937ab7749f1
Zudillo <fj.glezvillaverde@outlook.com>
2024-11-02 00:39:35 +0100

    Añadido mi nombre a Readme

---
+++ src/main/java/org/iesalandalus/programacion/damas/Direccion.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.damas;
+
+public enum Direccion {
+    NORESTE("Noreste"),
+    NOROESTE("Noroeste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste");
+
+
+
+
+
+}
