dff2c4ad467a6922a51622fd89c4d2fe7b7ae41a
Zudillo <fj.glezvillaverde@outlook.com>
2024-11-02 13:40:40 +0100

    Añadido mi nombre a Readme

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -4,12 +4,20 @@ public class Posicion {
     int fila;
     char columna;
 
+    public Posicion(int fila, char columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+
     public char getColumna() {
         return columna;
     }
 
     public void setColumna(char columna) {
-        this.columna = columna;
+        if (columna < a || columna > h) {
+            throw new IllegalArgumentException("La columna debe estar entre a y h");
+        }
+            this.columna = columna;
     }
 
     public int getFila() {
@@ -17,6 +25,9 @@ public class Posicion {
     }
 
     public void setFila(int fila) {
-        this.fila = fila;
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe estar entre 1 y 8");
+        }
+            this.fila = fila;
     }
 }
