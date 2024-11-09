d8034d91541d3cdc6c60295caa3d50ff2141ec4c
Zudillo <fj.glezvillaverde@outlook.com>
2024-11-09 14:20:38 +0100

    Nuevo cambio

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -5,4 +5,13 @@ public class Consola {
 
     public Consola() {
     }
+
+    public static void mostrarMenu() {
+        System.out.println("Menú de opciones:");
+        System.out.println("1. Crear dama por defecto.");
+        System.out.println("2. Crear dama eligiendo color." );
+        System.out.println("3. Mover.");
+        System.out.println("4. Salir.");
+    }
+
 }
